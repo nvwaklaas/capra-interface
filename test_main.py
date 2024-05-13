a47e702658e4fb8676b63c2fe8cae4fa3092409f
Thymen van Scheppingen <thymen@q2-software.nl>
2024-05-13 12:32:54 +0200

    Added Mockdatabase

--- test_main.py
+++ test_main.py
@@ -1,7 +1,26 @@
 """Pytest tests for testing the FastAPI for controlling Capra Hircus"""
+import sqlite3
 import pytest
 from fastapi.testclient import TestClient
 from main import app
+
+
+@pytest.fixture(name="db_session")
+def session():
+    """Create Mock database"""
+    connection = sqlite3.connect(':memory:')
+    db_session = connection.cursor()
+    yield db_session
+    connection.close()
+
+
+@pytest.fixture
+def setup_db(db_session):
+    """Prepare database for testing"""
+    db_session.execute('''CREATE TABLE instructions
+                          (angle float, speed int, distance float)''')
+
+    db_session.connection.commit()
 
 
 @pytest.fixture(name="client")
@@ -10,6 +29,7 @@ def test_client():
     yield TestClient(app)
 
 
+@pytest.mark.usefixtures("setup_db")
 def test_create_instruction(client):
     """Tests if instrctuctions can be created"""
     instruction_data = {"angle": 0.5, "speed": 1, "distance": 0.1}
@@ -18,6 +38,7 @@ def test_create_instruction(client):
     assert "id" in response.json()
 
 
+@pytest.mark.usefixtures("setup_db")
 def test_stop_robot(client):
     """Testing if stopping the robot results in the correct message"""
     response = client.post("/stop/")
@@ -25,6 +46,7 @@ def test_stop_robot(client):
     assert response.json() == {"message": "Robot stopped"}
 
 
+@pytest.mark.usefixtures("setup_db")
 def test_connection_failed(client):
     """Testing if the correct error message is returned when connection to the robot failed"""
     response = client.get("/connect_to_robot")
@@ -32,6 +54,7 @@ def test_connection_failed(client):
     assert response.json() == {'detail': 'Failed to connect to Capra Hircus'}
 
 
+@pytest.mark.usefixtures("setup_db")
 def test_upload_json(client):
     """Testing uploading JSOn files"""
     files = {
