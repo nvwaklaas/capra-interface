7880e28befbdd302821718ed370b88499c97986d
Thymen van Scheppingen <thymen@q2-software.nl>
2024-05-22 12:20:06 +0200

    #23 Added input validation to create_instruction

--- models/instruction_create.py
+++ models/instruction_create.py
@@ -1,5 +1,5 @@
 """Instruction Pydantic Model"""
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class InstructionCreate(BaseModel):
@@ -7,3 +7,27 @@ class InstructionCreate(BaseModel):
     angle: float = 0
     speed: int
     distance: float
+
+    @field_validator('angle')
+    @classmethod
+    def check_angle(cls, v):
+        """Validate angle field"""
+        if v < -0.8 or v > 1.5:
+            raise ValueError("Angle must be between -0.8 and 1.5 radians")
+        return v
+
+    @field_validator('speed')
+    @classmethod
+    def check_speed(cls, v):
+        """Validate speed field"""
+        if v < -2 or v > 2:
+            raise ValueError("Speed must be between -2 and 2 m/s")
+        return v
+
+    @field_validator('distance')
+    @classmethod
+    def check_distance(cls, v):
+        """Validate distance field"""
+        if v < 0.1 or v > 100:
+            raise ValueError("Distance must be between 0.1 and 100 meter")
+        return v
